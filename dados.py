8f1bbfefd2dcfac91baacbb010d755e5b415b370
luisfelipesdn12-email <luisfelipesdn12@gmail.com>
2020-02-24 00:23:13 +0000

    Organizei os beta-testers

--- dados.py
+++ dados.py
@@ -1,3 +1,9 @@
 competicoes_num = 1
 
-email_dos_destinatarios = ['luisfelipesdn12@gmail.com', 'luisfelipesantosllutv@gmail.com', 'zdarkbrz@gmail.com', 'zkanekiken@gmail.com', 'muka.game@gmail.com']
+email_dos_destinatarios = [
+  'luisfelipesdn12@gmail.com', 
+  'luisfelipesantosllutv@gmail.com', 
+  'zdarkbrz@gmail.com', 
+  'zkanekiken@gmail.com', 
+  'muka.game@gmail.com'
+  ]

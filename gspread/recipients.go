3e3e38233ab691f7ead67486cb4b5234b9b9219a
unknown <luisfelipesdn12@gmail.com>
2020-08-09 20:59:27 -0300

    User inputs with spaces in the extremities are now handled
    And imporve the documentaion of  file

--- gspread/recipients.go
+++ gspread/recipients.go
@@ -32,22 +32,39 @@ import (
 // number was checked. All this properties are not literal
 // values, are `spreadsheet.Cell` objects witch contains useful
 // information about the cell where the value is stored in
-// addition to the value itself
+// addition to the value itself.
 type RecipientStruct struct {
-	Name                        spreadsheet.Cell
-	Email                       spreadsheet.Cell
-	City                        spreadsheet.Cell
-	Language                    spreadsheet.Cell
-	UpcomingCompetitions        spreadsheet.Cell
-	LastVerification            spreadsheet.Cell
-	Sheet                       *spreadsheet.Sheet
+	// Basic data, for send emails and do the verifications.
+	Name  spreadsheet.Cell
+	Email spreadsheet.Cell
+	City  spreadsheet.Cell
+
+	// Language, for emails be responsive to it.
+	Language spreadsheet.Cell
+
+	// The upcoming competitions number in the last verification.
+	UpcomingCompetitions spreadsheet.Cell
+	// The date/time information in string of the last verification.
+	LastVerification spreadsheet.Cell
+
+	// The pointer to spreadsheet object itself, it helps
+	// to synchronize changes using just the `RecipientStruct`,
+	// by example.
+	Sheet *spreadsheet.Sheet
+
+	// The upcoming competitions number that was/will be
+	// checked in this exactly runtime. Can be a zero value
+	// if it was not checked yet.
 	CurrentUpcomingCompetitions int
-	CurrentVerificationDate     string
+	// The date/time information in string of the
+	// verification in this exactly runtime. Can be
+	// a zero value if it was not checked yet.
+	CurrentVerificationDate string
 }
 
 // GetRecipientsData fetch the data stored in the
 // spreadsheet, tranform it in a slice of `RecipientStruct`
-// as described above
+// as described above.
 func GetRecipientsData(spreadData spreadsheet.Spreadsheet) ([]RecipientStruct, error) {
 
 	recipients := []RecipientStruct{}
@@ -55,7 +72,7 @@ func GetRecipientsData(spreadData spreadsheet.Spreadsheet) ([]RecipientStruct, e
 	// Fetch the specific recipients sheet with the
 	// `spreadData` value. If an error happen, the
 	// function returns a empty slice of `RecipientStruct`
-	// and the error
+	// and the error.
 	log.Println(`Fetching the specific sheet "Recipients"`)
 	recipientsSheet, err := spreadData.SheetByTitle("Recipients")
 	if err != nil {
@@ -64,17 +81,22 @@ func GetRecipientsData(spreadData spreadsheet.Spreadsheet) ([]RecipientStruct, e
 
 	// Uses the exact recipients number to make a slice
 	// where the final `RecipientStruct` values will be
-	// stored
+	// stored.
 	recipients = make(
 		[]RecipientStruct, 0, len(recipientsSheet.Rows)-1,
 	)
 
 	// Tranform the each row in the `RecipientStruct`
-	// and append it to the `recipients` slice
+	// and append it to the `recipients` slice.
 	for row := 1; row < len(recipientsSheet.Rows); row++ {
 
+		// returns a slice of `spreadsheet.Cell` objects
+		// that cells are equivalent to the data of a
+		// specific recipient.
 		rowCells := recipientsSheet.Rows[row]
 
+		// Using the position in the spreadsheet,
+		// defina a `RecipientStruct`.
 		recipientData := RecipientStruct{
 			Name:                 rowCells[1],
 			Email:                rowCells[2],
@@ -85,9 +107,19 @@ func GetRecipientsData(spreadData spreadsheet.Spreadsheet) ([]RecipientStruct, e
 			Sheet:                recipientsSheet,
 		}
 
+		// The properties `Name`, `Email` and `City`
+		// are free user inputs, so it can be with
+		// spaces in extremities like " My Name" or
+		// "   My City " . It is very bad when the
+		// program sends an email or verify if a city
+		// already exists in `main.cityUpcomingCompetitionsCache`.
+		// So the lines below strip this strings using
+		// the function `stripIfNecessary`.
 		stripIfNecessary(&recipientData.Name.Value)
 		stripIfNecessary(&recipientData.Email.Value)
 		stripIfNecessary(&recipientData.City.Value)
+
+		// Add the `RecipientStruct` in the slice.
 		recipients = append(recipients, recipientData)
 	}
 
@@ -96,21 +128,29 @@ func GetRecipientsData(spreadData spreadsheet.Spreadsheet) ([]RecipientStruct, e
 
 // UpdateUpcomingCompetitions update the recipient date
 // in the spreadsheet with the new upcoming competitions
-// number and last verifications date
+// number and last verifications date.
 func (recipient RecipientStruct) UpdateUpcomingCompetitions() error {
+
+	log.Printf("Updating and Syncronyzing the upcoming competitions from %v\n", recipient.Name.Value)
+
+	// Update the `UpcomingCompetitions` propertie
+	// in the spreadsheet.
 	recipient.Sheet.Update(
 		int(recipient.UpcomingCompetitions.Row),
 		int(recipient.UpcomingCompetitions.Column),
 		strconv.Itoa(recipient.CurrentUpcomingCompetitions),
 	)
 
+	// Update the `LastVerification` propertie
+	// in the spreadsheet.
 	recipient.Sheet.Update(
 		int(recipient.LastVerification.Row),
 		int(recipient.LastVerification.Column),
 		string(recipient.CurrentVerificationDate),
 	)
 
-	log.Printf("Updating and Syncronyzing the upcoming competitions from %v\n", recipient.Name.Value)
+	// Synchronize the updates with the original
+	// spreadsheet in the Google account.
 	err := recipient.Sheet.Synchronize()
 	if err != nil {
 		return err
@@ -119,6 +159,8 @@ func (recipient RecipientStruct) UpdateUpcomingCompetitions() error {
 	return nil
 }
 
+// stripIfNecessary delete all spaces in the prefix
+// and suffix string, given it pointer.
 func stripIfNecessary(s *string) {
 	if strings.HasPrefix(*s, "") || strings.HasSuffix(*s, " ") {
 		*s = strings.TrimSpace(*s)
